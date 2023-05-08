e2e5c3667957ce1309aa6e1482c3d353b4051120
zinnnn37 <zinnnn37@gmail.com>
2023-05-09 04:21:30 +0900

    [QR] basic structure

---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import './List.scss'
+
+const List = () => {
+    return (
+        <div className="List">
+            <div className="List__container">상품 정보</div>
+            <div className="List__container">상품 정보</div>
+            <div className="List__container">상품 정보</div>
+        </div>
+    )
+}
+
+export default List
---
+++ src/components/QR.js
@@ -0,0 +1,11 @@
+import './QR.scss'
+
+const QR = () => {
+    return (
+        <div className="QR__Box">
+            <div className="QR__Code"></div>
+        </div>
+    )
+}
+
+export default QR
