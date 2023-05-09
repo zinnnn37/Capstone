3e547cf21967add7d67b75b7f42a8f8a42a4179d
zinnnn37 <zinnnn37@gmail.com>
2023-05-09 14:44:12 +0900

    ha

---
+++ src/pages/Form.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Form.scss'
+
+const Form = () => {
+    return (
+        <div className="Form">
+            <h1>test</h1>
+        </div>
+    )
+}
+
+export default Form
---
+++ src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import List from '../components/List'
+import QR from '../components/QR'
+
+const Main = () => {
+    return (
+        <div>
+            <List></List>
+            <QR></QR>
+        </div>
+    )
+}
+
+export default Main
