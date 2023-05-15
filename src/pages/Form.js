6a5417df3a0e9c2195dae341c7f17c253505c73a
zinnnn37 <zinnnn37@gmail.com>
2023-05-15 16:04:00 +0900

    [Form] react-hook-form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 import DatePicker from "react-datepicker";
 import "./Form.scss";
 
@@ -11,37 +12,56 @@ import "./Form.scss";
 
 const Form = () => {
     const [startDate, setStartDate] = useState(new Date());
+    const { register, handleSubmit } = useForm();
 
     return (
         <div className="background">
             <div className="FormContainer">
                 <div className="FormBack">
-                    <form className="Form">
+                    <form
+                        className="Form"
+                        id="form"
+                        onSubmit={handleSubmit(
+                            (data) => alert(JSON.stringify(data)) // 데이터 서버로 전달하는 함수
+                        )}
+                    >
                         <h1 className="FormTitle">정보 입력</h1>
                         <hr className="FormHr"></hr>
                         <div className="InputContainer">
                             <h3 className="InputIdx">ID</h3>
                             <input
                                 className="Input"
+                                id="id"
                                 type="text"
-                                //value={id}
+                                {...register("id", {
+                                    required: true,
+                                    minLength: 1,
+                                })}
                             ></input>
                             <h3 className="InputIdx">휴대전화</h3>
                             <input
                                 placeholder="'-' 없이 입력"
                                 className="Input"
+                                id="phone"
                                 type="text"
-                                //value={phone}
+                                {...register("phone", {
+                                    required: true,
+                                    minLength: 11,
+                                    maxLength: 11,
+                                })}
                             ></input>
                             <h3 className="InputIdx">공연 날짜</h3>
                             <DatePicker
                                 selected={startDate}
                                 onChange={(date) => setStartDate(date)}
+                                //{...register("Date", { required: true })}
                             />
                         </div>
                     </form>
                 </div>
-                <button className="Submit">제출</button>
+                <button type="submit" className="Submit" form="form">
+                    제출
+                </button>
             </div>
         </div>
     );
