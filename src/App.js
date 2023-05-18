35ea45708eacc4cb8885e1e2a7c33bfcd1066f72
zinnnn37 <zinnnn37@gmail.com>
2023-05-18 21:05:09 +0900

    [App.js] rm Test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Form from "./pages/Form";
-import Test from "./components/QRImg";
 
 const App = () => {
     return (
