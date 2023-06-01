ab541825196a6e3908dc149e3f1dfad9868ae3c9
zinnnn37 <zinnnn37@gmail.com>
2023-06-01 16:12:44 +0900

    [LIST] 길어지는 부분 ...으로

--- src/components/QRImg.js
+++ src/components/QRImg.js
@@ -2,7 +2,7 @@ import QRCodeComponent from "./QRCodeComponent";
 import "./QRImg.scss";
 
 function QRImg() {
-    const url = "192.168.0.10:3000/Form";
+    const url = "192.168.1.92:3000/Form";
 
     return (
         <div className="QR">
